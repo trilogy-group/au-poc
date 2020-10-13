fd9e618d13b933e9e31bebd2e92359a36df842d1
Raul Guerrero <raul.guerrero@devfactory.com>
2020-10-13 11:33:03 -0500

    begin XML

---
+++ au.js
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE html><html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <title>Architecture Update - template</title>
+  </head>
+  <body>
+    <p><br />
+    </p>
+  </body>
+</html>
